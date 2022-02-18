82352be2fce9e908a457d8cd524761e999e38813
arham5416 <md.arhamkhan09@gmail.com>
2022-02-18 13:08:21 +0530

    main

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { ApiService } from './api/api.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular-Library';
+
+
+  inputAuthor: string = ""
+  inputBook: string = ""
+  bookOutput: number = 0 
+  booksArray: Array<any> = []
+  isShow = false
+
+  constructor(
+    private apiService: ApiService,
+  ) {
+}
+
+public btnSearchClicked(): void {
+  if (this.inputBook.length >= 2 || this.inputAuthor.length >= 2) {
+    this.apiService.getBooks(this.inputBook, this.inputAuthor).subscribe((data) => {
+      console.log(data);
+      this.bookOutput = data['numFound'];
+      this.booksArray = data['docs'];
+      console.log(this.booksArray);
+      this.isShow = true;
+    })
+  }
+
+}
+ 
+  
+}
